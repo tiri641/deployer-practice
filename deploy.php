35e89a53984c67b967c4860191513f04b16fb581
tiri641 <myhearisranch@gmail.com>
2026-03-15 00:01:53 +0900

    メンテナンスモードのページを作成する

--- deploy.php
+++ deploy.php
@@ -1,19 +1,29 @@
 <?php
 namespace Deployer;
 
-require 'recipe/laravel.php';
+require 'recipe/common.php';
 
 // Config
-
-set('repository', '');
+set('repository', 'git@github.com:tiri641/deployer-practice.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Hosts
+host('18.183.159.210')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/my-app')
+    ->set('forward_agent', true);
 
+// Tasks
+task('maintenance:on', function () {
+    run('touch {{current_path}}/maintenance.flag');
+});
+
+task('maintenance:off', function () {
+    run('rm -f {{current_path}}/maintenance.flag');
+});
 
 // Hooks
-
 after('deploy:failed', 'deploy:unlock');
---
+++ maintenance.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>メンテナンス中</title>
+</head>
+<body>
+    <h1>メンテナンス中です</h1>
+    <p>しばらくお待ちください。</p>
+</body>
+</html>
